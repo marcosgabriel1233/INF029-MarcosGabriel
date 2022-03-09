19a39c7bed348eb1ff8602f2ccc13128e4165145
marcosgabriel1233 <mgoliveira2021@gmail.com>
2022-03-09 13:02:21 -0300

    Update prototipo2.c
    inserção dos professores, mas o programa para de funcionar quando tento cadastrar

--- ProjetoEscola/prototipo2.c
+++ ProjetoEscola/prototipo2.c
@@ -12,20 +12,30 @@ struct data
 
 struct Aluno
 {
-    int matricula;
-    char nome[50];
-    char sexo;
-    int cpf;
+    int Amatricula;
+    char Anome[50];
+    char Asexo;
+    int Acpf;
 };
+
+struct Professor
+{
+    int Pmatricula;
+    char Pnome[50];
+    char Psexo;
+    int Pcpf;
+};
+
 
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
 
-    struct Aluno cadastro[n];
+    struct Aluno Acadastro[n];
+    struct Professor Pcadastro[n];
     struct data cadData;
     int opcao;
-    int cadastrados;
+    int Acadastrados, Pcadastrados;
     int sair = 0;
 
     while (!sair)
@@ -34,7 +44,8 @@ int main(void)
         printf("Escolha a operação:\n");
         printf("0 - Sair\n");
         printf("1 - Inserir Aluno\n");
-        printf("2 - Listar alunos cadastrados\n");
+        printf("2 - Cadastrar Professor\n");
+        printf("3 - Listar alunos cadastrados\n");
         scanf("%d", &opcao);
 
         switch (opcao)
@@ -48,37 +59,74 @@ int main(void)
         case 1:
         {
             printf("Digite a matrícula: ");
-            scanf("%d", &cadastro[cadastrados].matricula);
+            scanf("%d", &Acadastro[Acadastrados].Amatricula);
             getchar();
 
             printf("Digite o nome: ");
-            fgets(cadastro[cadastrados].nome, 50, stdin);
-            size_t ln = strlen(cadastro[cadastrados].nome) - 1;
-            if (cadastro[cadastrados].nome[ln] == '\n')
-                cadastro[cadastrados].nome[ln] = '\0';
+            fgets(Acadastro[Acadastrados].Anome, 50, stdin);
+            size_t ln = strlen(Acadastro[Acadastrados].Anome) - 1;
+            if (Acadastro[Acadastrados].Anome[ln] == '\n')
+                Acadastro[Acadastrados].Anome[ln] = '\0';
 
             printf("Digite o sexo: ");
-            scanf("%c", &cadastro[cadastrados].sexo);
+            scanf("%c", &Acadastro[Acadastrados].Asexo);
 
             printf("Digite a data de nascimento (dd/mm/aa): ");
             scanf("%d/%d/%d", &cadData.dia, &cadData.mes, &cadData.ano);
             getchar();
 
             printf("Digite o CPF: ");
-            scanf("%d", &cadastro[cadastrados].cpf);
+            scanf("%d", &Acadastro[Acadastrados].Acpf);
             printf("\n");
 
-            cadastrados++;
+            Acadastrados++;
 
             break;
         }
-        case 2: 
+            case 2:
         {
-            for(int i = 0; i < cadastrados; i++)
+            printf("Digite a matrícula: ");
+            scanf("%d", &Pcadastro[Pcadastrados].Pmatricula);
+            getchar();
+
+            printf("Digite o nome: ");
+            fgets(Pcadastro[Pcadastrados].Pnome, 50, stdin);
+            size_t ln = strlen(Pcadastro[Pcadastrados].Pnome) - 1;
+            if (Pcadastro[Pcadastrados].Pnome[ln] == '\n')
+                Pcadastro[Pcadastrados].Pnome[ln] = '\0';
+
+            printf("Digite o sexo: ");
+            scanf("%c", &Pcadastro[Pcadastrados].Psexo);
+
+            printf("Digite a data de nascimento (dd/mm/aa): ");
+            scanf("%d/%d/%d", &cadData.dia, &cadData.mes, &cadData.ano);
+            getchar();
+
+            printf("Digite o CPF: ");
+            scanf("%d", &Pcadastro[Pcadastrados].Pcpf);
+            printf("\n");
+
+            Pcadastrados++;
+
+            break;
+        }
+
+        case 3: 
+        {
+            for(int i = 0; i < Acadastrados; i++)
             {
-                printf("Nome: %s\n", cadastro[i].nome);
-                printf("CPF: %d\n", cadastro[i].cpf);
-                printf("Matrícula: %d\n", cadastro[i].matricula);
+                printf("Nome: %s\n", Acadastro[i].Anome);
+                printf("CPF: %d\n", Acadastro[i].Acpf);
+                printf("Matrícula: %d\n", Acadastro[i].Amatricula);
+            }
+        }
+        case 4: 
+        {
+            for(int i = 0; i < Pcadastrados; i++)
+            {
+                printf("Nome: %s\n", Pcadastro[i].Pnome);
+                printf("CPF: %d\n", Pcadastro[i].Pcpf);
+                printf("Matrícula: %d\n", Pcadastro[i].Pmatricula);
             }
         }
         }
